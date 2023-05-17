96e44b21a539a24f770f849473ddcde33f273195
Frankie915 <frankmm083@gmail.com>
2023-05-17 18:55:54 -0400

    closes #2

---
+++ PracticeManagement/PracticeManagement/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.PracticeManagement.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public DateTime OpenDate { get; set; }
+
+        public DateTime CloseDate { get; set; }
+
+        public Boolean IsActive { get; set; }
+
+        public string? ShortName { get; set; }
+        
+        public string? LongName { get; set; }
+        public int ClientId { get; set; }
+    }
+}
---
+++ PracticeManagement/PracticeManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeManagement // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
