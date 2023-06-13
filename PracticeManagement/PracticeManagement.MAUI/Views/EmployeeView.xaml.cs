dd8486b417b4bd7ecc94dcf10fe5e6f5ef9c99e2
Frankie915 <frankmm083@gmail.com>
2023-06-13 14:49:07 -0400

    EmployeeView.xaml and .cs being updated. Template buttons added, no functionality yet

--- PracticeManagement/PracticeManagement.MAUI/Views/EmployeeView.xaml.cs
+++ PracticeManagement/PracticeManagement.MAUI/Views/EmployeeView.xaml.cs
@@ -17,20 +17,36 @@ public partial class EmployeeView : ContentPage
 
     private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
     {
-
+        (BindingContext as EmployeeViewViewModel).ResetView();
+        (BindingContext as EmployeeViewViewModel).RefreshView();
     }
 
     private void Toolbar_ClientsClicked(object sender, EventArgs e)
     {
-
+        (BindingContext as EmployeeViewViewModel).ShowClients();
     }
 
     private void Toolbar_ProjectsClicked(object sender, EventArgs e)
     {
-
+        (BindingContext as EmployeeViewViewModel).ShowProjects();
     }
 
     private void Toolbar_TeamClicked(object sender, EventArgs e)
+    {
+        (BindingContext as EmployeeViewViewModel).ShowTeam();
+    }
+
+    private void AddProjectClicked(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void EditProjectClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void RemoveProjectClicked(object sender, EventArgs e)
     {
 
     }
