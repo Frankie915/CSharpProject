c56c52adf9f8084f00d3ee90c7652f191411c67c
Frankie915 <frankmm083@gmail.com>
2023-06-30 22:17:38 -0400

    Fakedatabase created and added ClientService data ti the database. Closed #28 and closed #29

---
+++ PracticeManagement/PracticeManagement.API/PracticeManagement.API/Database/FakeDatabase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using PracticeManagement.CLI.Models;
+
+namespace PracticeManagement.API.Database
+{
+    public static class FakeDatabase
+    {
+        public static List<Client> clients = new List<Client>
+        {
+            new Client{Id = 1, Name = "Christian", Notes = "Unstable" },
+            new Client{Id = 2, Name = "Poox", Notes = "Smoll" },
+            new Client{Id = 3, Name = "Ness", Notes = "Autistic" }
+        };
+    }
+}
