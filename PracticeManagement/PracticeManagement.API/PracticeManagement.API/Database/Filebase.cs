5c1dbed194ce5ac36a0a1bf2a5681addb363f459
Frankie915 <frankmm083@gmail.com>
2023-07-10 18:08:48 -0400

    added Filebase.cs and updated ClientEC

---
+++ PracticeManagement/PracticeManagement.API/PracticeManagement.API/Database/Filebase.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using PracticeManagement.API.EC;
+using PracticeManagement.CLI.Models;
+using System.IO;
+
+namespace PracticeManagement.API.Database
+{
+    public class Filebase
+    {
+        private string _root;
+        private string _clientRoot;
+        private string _projectRoot;
+        private static Filebase? _instance;
+
+
+        public static Filebase Current
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Filebase();
+                }
+
+                return _instance;
+            }
+        }
+
+        private Filebase()
+        {
+            _root = "C:\\temp";
+            _clientRoot = $"{_root}\\Clients";
+            _projectRoot = $"{_root}\\Projects";
+            //todo add support for employees, times, and bills
+        }
+
+        private int LastClientId => Clients.Any() ? Clients.Select(c => c.Id).Max() : 0;
+        public Client AddOrUpdate(Client c)
+        {
+            //set up a new Id if one doesn't already exist
+            if(c.Id <= 0)
+            {
+                c.Id = LastClientId + 1;
+            }
+
+            var path = $"{_clientRoot}\\{c.Id}.json";
+
+            //if the item has been previously persisted
+            Delete(c.Id);
+
+            //var path = $"{_clientRoot}\\{id}.json";
+
+            //if (File.Exists(path))
+            //{
+            //    //blow it up
+            //    File.Delete(path);
+            //}
+
+            //write the file
+            File.WriteAllText(path, JsonConvert.SerializeObject(c));
+
+            //return the item, which now has an id
+            return c;
+        }
+
+        public List<Client> Clients
+        {
+            get
+            {
+                var root = new DirectoryInfo(_clientRoot);
+                var _clients = new List<Client>();
+                foreach (var clientFile in root.GetFiles())
+                {
+                    var client = JsonConvert.DeserializeObject<Client>(File.ReadAllText(clientFile.FullName));
+                    if(client != null)
+                    {
+                        _clients.Add(client);
+                    }
+
+                }
+                return _clients;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            var path = $"{_clientRoot}\\{id}.json";
+
+            if (File.Exists(path))
+            {
+                //blow it up
+                File.Delete(path);
+            }
+            return true;
+        }
+    }
+
+}
--- PracticeManagement/PracticeManagement.API/PracticeManagement.API/EC/ClientEC.cs
+++ PracticeManagement/PracticeManagement.API/PracticeManagement.API/EC/ClientEC.cs
@@ -9,42 +9,43 @@ namespace PracticeManagement.API.EC
     {
         public ClientDTO AddOrUpdate(ClientDTO dto)
         {
-            if (dto.Id > 0)
-            {
-                var clientToUpdate = FakeDatabase.Clients.FirstOrDefault(c => c.Id == dto.Id);
-                if (clientToUpdate != null)
-                {
-                    FakeDatabase.Clients.Remove(clientToUpdate);
-                }
-                FakeDatabase.Clients.Add(new Client(dto));
-            }
-            else
-            {
-                dto.Id = FakeDatabase.LastClientId + 1;
-                FakeDatabase.Clients.Add(new Client(dto));   
-            }
+            //    if (dto.Id > 0)
+            //    {
+            //        var clientToUpdate = Filebase.Current.Clients.FirstOrDefault(c => c.Id == dto.Id);
+            //        if (clientToUpdate != null)
+            //        {
+            //            Filebase.Current.Clients.Remove(clientToUpdate);
+            //        }
+            //        Filebase.Current.Clients.Add(new Client(dto));
+            //    }
+            //    else
+            //    {
+            //        Filebase.Current.Clients.Add(new Client(dto));   
+            //    }
+
+            Filebase.Current.AddOrUpdate(new Client(dto));
             return dto;
         }
 
         public ClientDTO? Get(int id)
         {
-            var returnVal = FakeDatabase.Clients.FirstOrDefault(c => c.Id == id) ?? new Client();
+            var returnVal = Filebase.Current.Clients.FirstOrDefault(c => c.Id == id) ?? new Client();
             return new ClientDTO(returnVal);
         }
 
         public ClientDTO? Delete(int id)
         {
-            var clientToDelete = FakeDatabase.Clients.FirstOrDefault(c => c.Id == id);
+            var clientToDelete = Filebase.Current.Clients.FirstOrDefault(c => c.Id == id);
             if (clientToDelete != null)
             {
-                FakeDatabase.Clients.Remove(clientToDelete);
+                Filebase.Current.Clients.Remove(clientToDelete);
             }
             return clientToDelete != null ? new ClientDTO(clientToDelete) : null;
         }
 
         public IEnumerable<ClientDTO> Search(string query ="")
         {
-            return FakeDatabase.Clients.Where(c => c.Name.ToUpper().Contains(query.ToUpper())).Take(1000).Select(c => new ClientDTO(c));
+            return Filebase.Current.Clients.Where(c => c.Name.ToUpper().Contains(query.ToUpper())).Take(1000).Select(c => new ClientDTO(c));
         }
     }
 }
